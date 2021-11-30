d8ed78193599432c3507af2cf6f4250e9226ef5a
bk <33919755+bkiscool@users.noreply.github.com>
2021-11-30 09:21:09 +0000

    Add files via upload

---
+++ src/net/pgfmc/core/Main.java
@@ -0,0 +1,21 @@
+package net.pgfmc.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+	
+	
+
+}
