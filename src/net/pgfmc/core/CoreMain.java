4d47b4730cb6372b5e57f5d47daacd6941b4cd25
bk <bk123yt@gmail.com>
2021-12-20 01:12:35 -0600

    Nice

--- src/net/pgfmc/core/CoreMain.java
+++ src/net/pgfmc/core/CoreMain.java
@@ -21,16 +21,7 @@ import net.pgfmc.core.backup.Backupconfirm;
 import net.pgfmc.core.backup.Restore;
 import net.pgfmc.core.cmd.Blocked;
 import net.pgfmc.core.cmd.Goto;
-import net.pgfmc.core.cmd.admin.Day;
-import net.pgfmc.core.cmd.admin.Debug;
-import net.pgfmc.core.cmd.admin.DimToggle;
-import net.pgfmc.core.cmd.admin.Fly;
-import net.pgfmc.core.cmd.admin.Gamemode;
-import net.pgfmc.core.cmd.admin.God;
-import net.pgfmc.core.cmd.admin.Heal;
 import net.pgfmc.core.cmd.admin.Skull;
-import net.pgfmc.core.cmd.admin.Sudo;
-import net.pgfmc.core.cmd.admin.Vanish;
 import net.pgfmc.core.cmd.donator.Nick;
 import net.pgfmc.core.configify.ReloadConfigify;
 import net.pgfmc.core.inventoryAPI.InventoryPressEvent;
@@ -161,34 +152,12 @@ public class CoreMain extends JavaPlugin implements Listener {
 		// getCommand("goto").setAliases(DimManager.getAllWorldNames()); // /hub, /creative, /<world>
 		
 		
-		
-		
-		getCommand("gmc").setExecutor(new Gamemode());
-		getCommand("gms").setExecutor(new Gamemode());
-		getCommand("gma").setExecutor(new Gamemode());
-		getCommand("gmsp").setExecutor(new Gamemode());
-		
-		getCommand("vanish").setExecutor(new Vanish());
-
-		getCommand("fly").setExecutor(new Fly());
-		getCommand("god").setExecutor(new God());
-		getCommand("sudo").setExecutor(new Sudo());
-		getCommand("heal").setExecutor(new Heal());
-		
 		getCommand("block").setExecutor(new Blocked());
-		
-		
-		getCommand("debug").setExecutor(new Debug());
-		
-		
+
 		
 		getCommand("backup").setExecutor(new Backup());
 		getCommand("backupconfirm").setExecutor(new Backupconfirm());
 		getCommand("restore").setExecutor(new Restore());
-		
-		getCommand("day").setExecutor(new Day());
-		
-		getCommand("dimtoggle").setExecutor(new DimToggle());
 		
 		getCommand("nick").setExecutor(new Nick());
 		
@@ -200,9 +169,6 @@ public class CoreMain extends JavaPlugin implements Listener {
 		
 		
 		getServer().getPluginManager().registerEvents(new InventoryPressEvent(), this);
-		getServer().getPluginManager().registerEvents(new Fly(), this);
-		getServer().getPluginManager().registerEvents(new God(), this);
-		getServer().getPluginManager().registerEvents(new Vanish(), this);
 		getServer().getPluginManager().registerEvents(new Permissions(), this);
 		
 		getServer().getPluginManager().registerEvents(this, this);
