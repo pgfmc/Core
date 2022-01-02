54c7cd40c725a3b20538047b8694e9beb3c66e87
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2022-01-02 02:43:48 -0600

    aslkdfhjl;a
    Module

--- src/net/pgfmc/core/requestAPI/Requester.java
+++ src/net/pgfmc/core/requestAPI/Requester.java
@@ -1,6 +1,5 @@
 package net.pgfmc.core.requestAPI;
 
-import java.net.http.WebSocket.Listener;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,6 +10,7 @@ import java.util.stream.Collectors;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import net.pgfmc.core.CoreMain;
