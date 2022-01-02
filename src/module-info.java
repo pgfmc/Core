c3f07ccba3d3e7a26afbc821c11973fd03418dd1
bk <bk123yt@gmail.com>
2022-01-02 06:49:36 -0600

    module info

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+module core {
+	exports net.pgfmc.core.requestAPI;
+	exports net.pgfmc.core.configify;
+	exports net.pgfmc.core.cmd.admin;
+	exports net.pgfmc.core.playerdataAPI;
+	exports net.pgfmc.core.cmd.donator;
+	exports net.pgfmc.core;
+	exports net.pgfmc.core.permissions;
+	exports net.pgfmc.core.cmd;
+	exports net.pgfmc.core.inventoryAPI;
+	exports net.pgfmc.core.report;
+
+	requires java.logging;
+	requires org.bukkit;
+}
